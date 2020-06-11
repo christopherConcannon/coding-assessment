3e8ae808183c3962073d93d9a87f563b63a54180
xtophe xanon <xocon10@gmail.com>
2020-06-10 20:22:50 -0500

    show feedback msg, clear screen after timeout and load next question

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,6 +41,8 @@ var questionsArr = [
 		correctAnswerIndex : 1
 	}
 ];
+var questionIndex = 0;
+
 
 // EVENT LISTENERS
 highScoreLink.addEventListener('click', highScoresPage);
@@ -54,9 +56,7 @@ function startQuiz() {
 	timer();
 
 	// call function to create single question page
-	// createQuestion(questionsArr[1]);
-	var i = 0;
-	createQuestion(questionsArr[i]);
+	createQuestion(questionsArr[questionIndex]);
 }
 
 function timer() {
@@ -72,9 +72,13 @@ function timer() {
 
 // FUNCTION TO CREATE SINGLE QUESTION PAGE
 function createQuestion(questionObj) {
+
+  
 	// create question page container
 	var questionContainer = document.createElement('div');
-	questionContainer.className = 'question';
+  questionContainer.className = 'question';
+  questionContainer.id = 'question';
+  
 	// create question
 	var question = document.createElement('h2');
 	question.textContent = questionObj.question;
@@ -83,12 +87,14 @@ function createQuestion(questionObj) {
 	// create answer-list
 	var answerList = document.createElement('ul');
 	answerList.className = 'answer-list';
+	answerList.id = 'answer-list';
 
 	// create array of answers
 	var answers = questionObj.answers;
 	// generate li for each answer in array
 	for (var i = 0; i < answers.length; i++) {
-		var answer = document.createElement('li');
+    var answer = document.createElement('li');
+    answer.className = 'answer-list-item';
 		// add button
 		answer.innerHTML =
 			'<button class="btn answer-btn">' + (i + 1) + '. ' + answers[i] + '</button>';
@@ -107,29 +113,54 @@ function createQuestion(questionObj) {
 
   answerList.addEventListener('click', checkAnswer);
   
-  function checkAnswer(event) {
-    var clicked = event.target.closest('li');
-    if (clicked.hasAttribute('data-correct-answer')){
+  // function checkAnswer(event) {
+  //   var clicked = event.target.closest('li');
+  //   if (clicked.hasAttribute('data-correct-answer')){
       
-      displayCorrect(answerList);
+  //     displayCorrect(answerList);
+  //   } else {
+  //     displayWrong(answerList);
+  //   }
+  // }
+
+}
+
+// CHECK ANSWER, DISPLAY FEEDBACK MSG, TIMED CALL TO NEXT QUESTION FUNCTION
+function checkAnswer(event) {
+  // find parent li of clicked button
+  console.log(event.target);
+  var clicked = event.target.closest('li.answer-list-item');
+  var answerList = document.getElementById('answer-list');
+ 
+  // check if clicked is truthy otherwise, ul was clicked and we don't want function to proceed
+  if (clicked) {
+    var isCorrectAnswer = (clicked.hasAttribute('data-correct-answer'));
+    if (isCorrectAnswer) {
+      // display correct message
+      var correctMsgEl = document.createElement('p');
+      correctMsgEl.className = 'feedback-msg';
+      correctMsgEl.innerText = 'Correct!'
+      answerList.appendChild(correctMsgEl); 
     } else {
-      displayWrong(answerList);
+      // display wrong message
+      var wrongMsgEl = document.createElement('p');
+      wrongMsgEl.className = 'feedback-msg';
+      wrongMsgEl.innerText = 'Wrong!'
+      answerList.appendChild(wrongMsgEl);
     }
+    answerList.removeEventListener('click', checkAnswer);
+  
+    var delayNextQuestion = setTimeout(function() {
+      var questionContainer = document.getElementById('question');
+      questionContainer.remove();
+      questionIndex++;
+      createQuestion(questionsArr[questionIndex]);
+    }, 1000)
   }
-}
+  
 
-function displayCorrect(answerList) {
-  var correctMsgEl = document.createElement('p');
-  correctMsgEl.className = 'feedback-msg';
-  correctMsgEl.innerText = 'Correct!'
-  answerList.appendChild(correctMsgEl);
-}
 
-function displayWrong(answerList) {
-  var wrongMsgEl = document.createElement('p');
-  wrongMsgEl.className = 'feedback-msg';
-  wrongMsgEl.innerText = 'Wrong!'
-  answerList.appendChild(wrongMsgEl);
+  // QUESTION...DO I NEED TO CLEAR THIS TIMEOUT?
 }
 
 // FUNCTION TO CREATE GAME OVER PAGE
